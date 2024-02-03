d8dcbe0c2217e5c13b7fd099924409bc8d16f0f5
fleamon <fleamon@nate.com>
2024-02-03 15:04:33 +0900

    update

--- CommonFunction.py
+++ CommonFunction.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import datetime
+from datetime import datetime, timedelta
 import yaml
 import time
 import pandas as pd
@@ -19,6 +19,7 @@ with open(f'{path}/env/config.yaml', encoding='UTF-8') as f:
     _cfg = yaml.load(f, Loader=yaml.FullLoader)
 
 DISCORD_WEBHOOK_URL = _cfg['DISCORD_WEBHOOK_URL']
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.4459.183 Safari/537.36"}
 
 
 def connect_to_db():
@@ -86,17 +87,26 @@ def crawling_articles_from_keyword():
     """뉴스기사 DB 저장"""
     print([time.strftime('%Y-%m-%d %H:%M:%S')], "!!!!! DEBUG PRINT : 뉴스기사 DB 저장 !!!!!")
     try:
-        # maxpage = "1000"
         maxpage = "1"
-        querys = ["카카오+김범수"]
-        # querys = ["카카오+김범수", "삼성전자+이재용"]
+        # querys = ["카카오+김범수"]
+        # querys = ["삼성전자+이재용"]
+        querys = ["카카오+김범수", "삼성전자+이재용"]
         sort = "0"
-        start_date = "2023.01.01"
-        end_date = "2024.01.01"
+        start_date = "2024.01.01"
+        end_date = "2023.01.01"
         
         for i in range(0, len(querys)):
             print (querys[i])
-            naver_news_crawler(maxpage, querys[i], sort, start_date, end_date) 
+            start_datetime = datetime.strptime(start_date, "%Y.%m.%d")
+            end_datetime = datetime.strptime(end_date, "%Y.%m.%d")
+            # 시작 날짜부터 종료 날짜까지 하루씩 감소
+            current_datetime = start_datetime
+            while current_datetime >= end_datetime:
+                crawling_date_id = str(current_datetime.strftime("%Y.%m.%d"))
+                current_datetime -= timedelta(days=1)
+                naver_news_crawler(maxpage, querys[i], sort, crawling_date_id) 
+                print ("5 seconds sleep...")
+                time.sleep(5)
 
         send_message("KOR", "뉴스기사 DB 저장 success!")
         print("뉴스기사 DB 저장 successfully.")
@@ -106,7 +116,7 @@ def crawling_articles_from_keyword():
         time.sleep(1)
 
 
-def naver_news_crawler(maxpage, query, sort, start_date, end_date):
+def naver_news_crawler(maxpage, query, sort, crawling_date_id):
     # 각 크롤링 결과 저장하기 위한 리스트 선언 
     article_reg_date=[]
     article_link=[]
@@ -115,14 +125,15 @@ def naver_news_crawler(maxpage, query, sort, start_date, end_date):
     article_text=[]
     result={}
 
-    s_from = start_date.replace(".","")
-    e_to = end_date.replace(".","")
+    date_id = crawling_date_id.replace(".","")
     page = 1  
     maxpage =(int(maxpage)-1)*10+1  # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
     
     while page <= maxpage:
-        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + start_date + "&de=" + end_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
-        original_html = requests.get(url)
+        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + crawling_date_id + "&de=" + crawling_date_id + "&nso=so%3Ar%2Cp%3Afrom" + date_id + "to" + date_id + "%2Ca%3A&start=" + str(page)
+        print ("url :", url)
+        original_html = requests.get(url, headers=headers)
+        print ("original_html status : ", original_html)
         html = BeautifulSoup(original_html.text, "html.parser")
         
         url_info = html.select("div.group_news > ul.list_news > li div.news_area > div.news_info > div.info_group > a.info")
@@ -132,9 +143,10 @@ def naver_news_crawler(maxpage, query, sort, start_date, end_date):
                 article_link.append(href)
         
         page = page + 10
-
+    
     for naver_url in article_link:
-        naver_original_html = requests.get(naver_url)
+        naver_original_html = requests.get(naver_url, headers=headers)
+        print ("naver_original_html status : ", naver_original_html)
         naver_html = BeautifulSoup(naver_original_html.text, "html.parser")
 
         # 기사 발행일
@@ -155,9 +167,7 @@ def naver_news_crawler(maxpage, query, sort, start_date, end_date):
         if article == []:
             article = naver_html.select("#articeBody")
         article = ''.join(str(article))
-        # print (article)
-        # print (type(article))
-
+        
         # html태그제거 및 텍스트 다듬기
         pattern1 = '<[^>]*>'
         title = re.sub(pattern=pattern1, repl='', string=str(title))
@@ -178,23 +188,20 @@ def naver_news_crawler(maxpage, query, sort, start_date, end_date):
 
         titles.append(title)
         article_text.append(article)
-        # print ("============================================================")
-        # print (article)
-        # print ("============================================================")
-
+        
     result= {
-                "article_reg_date" : article_reg_date
-                , "article_link": article_link
-                , "company_name": query.split("+")[0]
-                , "news_agency" : news_agency
-                , "titles" : titles
-                , "article_text": article_text
+            "article_reg_date" : article_reg_date
+            , "article_link": article_link
+            , "company_name": query.split("+")[0]
+            , "news_agency" : news_agency
+            , "titles" : titles
+            , "article_text": article_text
             }
     df = pd.DataFrame(result)  # df로 변환
     df = df.drop_duplicates()
 
     # # 새로 만들 파일이름 지정
-    # now = datetime.datetime.now()  # 파일이름 현 시간으로 저장하기
+    # now = datetime.now()  # 파일이름 현 시간으로 저장하기
     # outputFileName = f'articles_from_naver_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.xlsx'
     # df.to_excel('/Users/kakao/Downloads/'+outputFileName,sheet_name='sheet1')
 
@@ -203,9 +210,6 @@ def naver_news_crawler(maxpage, query, sort, start_date, end_date):
     cursor = conn.cursor()
 
     for index, row in df.iterrows():
-        print (row["titles"])
-        print (row["article_text"])
-        print ("================================================")
         insert_query = f'''
             INSERT INTO stock_Korean_by_ESG_BackData.news_articles 
             (article_reg_date, article_link, company_name, news_agency, title, article_text, load_date)
@@ -227,9 +231,9 @@ def naver_news_crawler(maxpage, query, sort, start_date, end_date):
 
 def send_message(market, msg):
     """디스코드 메세지 전송"""
-    now = datetime.datetime.now()
+    now = datetime.now()
     if market == "KOR":
-        market_now = datetime.datetime.now(timezone('Asia/Seoul'))  # 한국 기준 현재 시간
+        market_now = datetime.now(timezone('Asia/Seoul'))  # 한국 기준 현재 시간
     elif market == "ERROR":
         market_now = now  # 에러용
 
