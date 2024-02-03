23f0c67af9dd9ba313f226ff32bfac94e1c4e337
fleamon <fleamon@nate.com>
2024-02-03 14:01:19 +0900

    update

--- CommonFunction.py
+++ CommonFunction.py
@@ -1,16 +1,15 @@
 import os
 import requests
-import json
 import datetime
 import yaml
 import time
 import pandas as pd
-import urllib.request
-import ssl
-import zipfile
 import pymysql
 from pytz import timezone
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
+import re
+from tqdm import tqdm
+
 
 path = os.getcwd()
 if 'ec2-user' in path:
@@ -39,7 +38,7 @@ def insert_Korean_all_code_info_to_DB():
     try:
         # 한국 모든 종목 정보
         Korean_all_code_file = pd.read_csv(f'{path}/csv_file/Korean_all_code.csv', encoding='CP949')
-        print (Korean_all_code_file)
+        # print (Korean_all_code_file)
 
         conn = connect_to_db()
         cursor = conn.cursor()
@@ -50,8 +49,6 @@ def insert_Korean_all_code_info_to_DB():
         conn.commit()
         
         for index, row in Korean_all_code_file.iterrows():
-            # print ("chk!", index)
-            # print ("chk!", row)
             sql = """
             INSERT INTO stock_Korean_by_ESG_BackData.Korean_all_code_info (standard_code, short_code, Korean_name, Korean_short_name,
             English_name, stock_reg_date_id, market_type, bond_type, attach_part, stock_type, reg_price, shares_number)
@@ -85,6 +82,149 @@ def insert_Korean_all_code_info_to_DB():
         time.sleep(1)
 
 
+def crawling_articles_from_keyword():
+    """뉴스기사 DB 저장"""
+    print([time.strftime('%Y-%m-%d %H:%M:%S')], "!!!!! DEBUG PRINT : 뉴스기사 DB 저장 !!!!!")
+    try:
+        # maxpage = "1000"
+        maxpage = "1"
+        querys = ["카카오+김범수"]
+        # querys = ["카카오+김범수", "삼성전자+이재용"]
+        sort = "0"
+        start_date = "2023.01.01"
+        end_date = "2024.01.01"
+        
+        for i in range(0, len(querys)):
+            print (querys[i])
+            naver_news_crawler(maxpage, querys[i], sort, start_date, end_date) 
+
+        send_message("KOR", "뉴스기사 DB 저장 success!")
+        print("뉴스기사 DB 저장 successfully.")
+        
+    except Exception as e:
+        send_message("ERROR", f"뉴스기사 DB 저장 [오류 발생]{e}")
+        time.sleep(1)
+
+
+def naver_news_crawler(maxpage, query, sort, start_date, end_date):
+    # 각 크롤링 결과 저장하기 위한 리스트 선언 
+    article_reg_date=[]
+    article_link=[]
+    news_agency=[]
+    titles=[]
+    article_text=[]
+    result={}
+
+    s_from = start_date.replace(".","")
+    e_to = end_date.replace(".","")
+    page = 1  
+    maxpage =(int(maxpage)-1)*10+1  # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
+    
+    while page <= maxpage:
+        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + start_date + "&de=" + end_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
+        original_html = requests.get(url)
+        html = BeautifulSoup(original_html.text, "html.parser")
+        
+        url_info = html.select("div.group_news > ul.list_news > li div.news_area > div.news_info > div.info_group > a.info")
+        for element in url_info:
+            href = element.get("href")
+            if "news.naver.com" in href:
+                article_link.append(href)
+        
+        page = page + 10
+
+    for naver_url in article_link:
+        naver_original_html = requests.get(naver_url)
+        naver_html = BeautifulSoup(naver_original_html.text, "html.parser")
+
+        # 기사 발행일
+        html_date = naver_html.select_one("div#ct> div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span")
+        news_date = html_date.attrs['data-date-time']
+        article_reg_date.append(news_date)
+
+        # 언론사
+        news_agency.append( naver_html.select_one('meta:nth-child(10)').get("content") )
+
+        # 뉴스 제목
+        title = naver_html.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_title > h2")
+        if title == None:
+            title = naver_html.select_one("#content > div.end_ct > div > h2")
+
+        # 뉴스 본문
+        article = naver_html.select("article#dic_area")
+        if article == []:
+            article = naver_html.select("#articeBody")
+        article = ''.join(str(article))
+        # print (article)
+        # print (type(article))
+
+        # html태그제거 및 텍스트 다듬기
+        pattern1 = '<[^>]*>'
+        title = re.sub(pattern=pattern1, repl='', string=str(title))
+        title = re.sub(r'[^\w\s]', '', title)
+        title = title.replace("'", "")
+        title = title.replace('"', "")
+        title = re.sub(r'[\[\]\(\)]', '', title)
+        title = re.sub(r'[\n\t]', '', title)
+
+        article = re.sub(pattern=pattern1, repl='', string=article)
+        pattern2 = """[\n\n\n\n\n// flash 오류를 우회하기 위한 함수 추가\nfunction _flash_removeCallback() {}"""
+        article = article.replace(pattern2, '')
+        article = re.sub(r'[^\w\s]', '', article)
+        article = article.replace("'", "")
+        article = article.replace('"', "")
+        article = re.sub(r'[\[\]\(\)]', '', article)
+        article = re.sub(r'[\n\t]', '', article)
+
+        titles.append(title)
+        article_text.append(article)
+        # print ("============================================================")
+        # print (article)
+        # print ("============================================================")
+
+    result= {
+                "article_reg_date" : article_reg_date
+                , "article_link": article_link
+                , "company_name": query.split("+")[0]
+                , "news_agency" : news_agency
+                , "titles" : titles
+                , "article_text": article_text
+            }
+    df = pd.DataFrame(result)  # df로 변환
+    df = df.drop_duplicates()
+
+    # # 새로 만들 파일이름 지정
+    # now = datetime.datetime.now()  # 파일이름 현 시간으로 저장하기
+    # outputFileName = f'articles_from_naver_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.xlsx'
+    # df.to_excel('/Users/kakao/Downloads/'+outputFileName,sheet_name='sheet1')
+
+    # DB에 insert
+    conn = connect_to_db()
+    cursor = conn.cursor()
+
+    for index, row in df.iterrows():
+        print (row["titles"])
+        print (row["article_text"])
+        print ("================================================")
+        insert_query = f'''
+            INSERT INTO stock_Korean_by_ESG_BackData.news_articles 
+            (article_reg_date, article_link, company_name, news_agency, title, article_text, load_date)
+            VALUES 
+            ('{row["article_reg_date"]}', '{row["article_link"]}'
+            , '{row["company_name"]}', '{row["news_agency"]}'
+            , '{row["titles"]}', '{row["article_text"]}', NOW())
+            ON DUPLICATE KEY UPDATE 
+            article_link=VALUES(article_link), 
+            title=VALUES(title), 
+            article_text=VALUES(article_text)
+        '''
+
+        cursor.execute(insert_query)
+        
+    conn.commit()
+    conn.close()
+    
+
 def send_message(market, msg):
     """디스코드 메세지 전송"""
     now = datetime.datetime.now()
@@ -96,4 +236,3 @@ def send_message(market, msg):
     message = {"content": f"[{market}]시간 : [{market_now.strftime('%Y-%m-%d %H:%M:%S')}] ==> {str(msg)}"}
     requests.post(DISCORD_WEBHOOK_URL, data=message)
     print([time.strftime('%Y-%m-%d %H:%M:%S')], message)
-
--- DailyMain.py
+++ DailyMain.py
@@ -7,8 +7,9 @@ import CommonFunction as cf
 
 
 def DailyMain():
-    cf.insert_Korean_all_code_info_to_DB()  # 종목정보 저장 (csv 파일은 일단 수동 저장필요함)
-    
+    # cf.insert_Korean_all_code_info_to_DB()  # 종목정보 저장 (csv 파일은 일단 수동 저장필요함)
+    cf.crawling_articles_from_keyword()  # 뉴스기사 crawling
+
 
 if __name__ == '__main__':
     DailyMain()
