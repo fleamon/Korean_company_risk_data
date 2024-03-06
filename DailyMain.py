ae9e8260422e4a48e80590e4608d3f588bad302f
fleamon <fleamon@nate.com>
2024-03-07 02:39:00 +0900

    get_company_ceo_name 추가 모든 search_keyword 확인하도록 수정 기사 저장에 포탈이름 추가 다음크롤링 틀잡기

--- DailyMain.py
+++ DailyMain.py
@@ -10,27 +10,23 @@ import ScoringByKeyword.keyword_scoring as keyword_scoring
 
 
 def DailyMain():
+    yesterday = datetime.now() - timedelta(days=1)
+    formatted_yesterday = yesterday.strftime('%Y.%m.%d')
+
     '''
     # 종목정보 저장 (csv 파일은 일단 수동 저장필요함)
     cf.insert_Korean_all_code_info_to_DB()
     '''
 
-    conn = cf.connect_to_db()
-    cursor = conn.cursor()
+    # 뉴스기사 crawling (네이버, 다음)
+    company_ceo_name_list = crawl.get_company_ceo_name()
+    company_ceo_name_list = company_ceo_name_list[0:10]
+    for company_ceo_name in company_ceo_name_list:
+        # start_date = "2022.07.02"
+        # end_date = "2022.07.01"
+        is_public = True
+        crawl.crawling_articles_from_keyword(company_ceo_name, formatted_yesterday, formatted_yesterday, is_public)
 
-    query = f'''
-        DELETE FROM stock_Korean_by_ESG_BackData.news_articles
-            WHERE article_reg_date = '{crawling_date_id.replace(".","-")}'
-            AND search_keyword = '{query}'
-            AND company_name = '{query.split("+")[0]}'
-    '''
-    # 네이버 뉴스기사 crawling
-    querys = ["토스+이승건"]
-    start_date = "2022.07.02"
-    end_date = "2022.07.01"
-    is_public = True
-    crawl.crawling_articles_from_keyword(querys, start_date, end_date, is_public)
-    
     """
     # bert 스코어링 결과 저장
     bert_scoring.main()
