bf46280c3911dbd8f16bc56fee553ee6fc9afce6
fleamon <fleamon@nate.com>
2024-03-17 07:08:45 +0900

    add daum article

--- CrawlingArticle/CrawlingFunction.py
+++ CrawlingArticle/CrawlingFunction.py
@@ -17,7 +17,8 @@ def crawling_articles_from_keyword(query, start_date, end_date, is_public):
     print([time.strftime('%Y-%m-%d %H:%M:%S')], "!!!!! DEBUG PRINT : 뉴스기사 DB 저장 !!!!!")
     try:
         maxpage = "1"  # 검색어 넣은 마지막 페이지
-        sort = "0"  # 0:관련도순, 1:최신순, 2:오래된순
+        naver_sort = "0"  # 0:관련도순, 1:최신순, 2:오래된순
+        daum_sort = "accuracy"  # accuracy:정확도순, recency:최신순, old:오래된순
         print (query)
         start_datetime = datetime.strptime(start_date, "%Y.%m.%d")
         end_datetime = datetime.strptime(end_date, "%Y.%m.%d")
@@ -26,8 +27,8 @@ def crawling_articles_from_keyword(query, start_date, end_date, is_public):
         while current_datetime >= end_datetime:
             crawling_date_id = str(current_datetime.strftime("%Y.%m.%d"))
             current_datetime -= timedelta(days=1)
-            naver_news_crawler(maxpage, query, sort, crawling_date_id, '네이버') 
-            # daum_news_crawler(maxpage, query, sort, crawling_date_id, '다음') 
+            naver_news_crawler(maxpage, query, naver_sort, crawling_date_id, '네이버') 
+            daum_news_crawler(maxpage, query, daum_sort, crawling_date_id, '다음') 
             print ("5 seconds sleep...")
             time.sleep(5)
 
@@ -39,7 +40,7 @@ def crawling_articles_from_keyword(query, start_date, end_date, is_public):
         time.sleep(1)
 
 
-def naver_news_crawler(maxpage, query, sort, crawling_date_id, portal_name):
+def naver_news_crawler(maxpage, query, naver_sort, crawling_date_id, portal_name):
     # 각 크롤링 결과 저장하기 위한 리스트 선언 
     article_reg_date=[]
     article_link=[]
@@ -53,7 +54,7 @@ def naver_news_crawler(maxpage, query, sort, crawling_date_id, portal_name):
     maxpage =(int(maxpage)-1)*10+1  # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
     
     while page <= maxpage:
-        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + crawling_date_id + "&de=" + crawling_date_id + "&nso=so%3Ar%2Cp%3Afrom" + date_id + "to" + date_id + "%2Ca%3A&start=" + str(page)
+        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+naver_sort+"&ds=" + crawling_date_id + "&de=" + crawling_date_id + "&nso=so%3Ar%2Cp%3Afrom" + date_id + "to" + date_id + "%2Ca%3A&start=" + str(page)
         print ("url :", url)
         original_html = requests.get(url, headers=cf.headers)
         print ("original_html status : ", original_html)
@@ -172,7 +173,7 @@ def naver_news_crawler(maxpage, query, sort, crawling_date_id, portal_name):
     conn.close()
 
 
-def daum_news_crawler(maxpage, query, sort, crawling_date_id, portal_name):
+def daum_news_crawler(maxpage, query, daum_sort, crawling_date_id, portal_name):
     # 각 크롤링 결과 저장하기 위한 리스트 선언 
     article_reg_date=[]
     article_link=[]
@@ -180,77 +181,58 @@ def daum_news_crawler(maxpage, query, sort, crawling_date_id, portal_name):
     titles=[]
     article_text=[]
     result={}
-
     date_id = crawling_date_id.replace(".","")
-    page = 1  
-    maxpage =(int(maxpage)-1)*10+1  # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
+    page = 1
+    # html태그제거 및 텍스트 다듬기
+    pattern1 = '<[^>]*>'
+    pattern2 = '"'
+    pattern3 = "'"
+    pattern4 = '\t'
     
-    while page <= maxpage:
-        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + crawling_date_id + "&de=" + crawling_date_id + "&nso=so%3Ar%2Cp%3Afrom" + date_id + "to" + date_id + "%2Ca%3A&start=" + str(page)
+    while page <= int(maxpage):
+        url = 'https://search.daum.net/search?w=news&nil_search=btn&DA=PGD&enc=utf8&cluster=y&cluster_page=1&q=' + query + '&sd=' + date_id + '000000&ed=' + date_id + '235959&period=u&p=' + str(page) + '&sort=' + daum_sort
         print ("url :", url)
         original_html = requests.get(url, headers=cf.headers)
+        page = page + 1
         print ("original_html status : ", original_html)
         html = BeautifulSoup(original_html.text, "html.parser")
-        
-        url_info = html.select("div.group_news > ul.list_news > li div.news_area > div.news_info > div.info_group > a.info")
-        for element in url_info:
-            href = element.get("href")
-            if "news.naver.com" in href:
-                article_link.append(href)
-        
-        page = page + 10
-    
-    to_remove_url = []
-    
-    for naver_url in article_link:
-        naver_original_html = requests.get(naver_url, headers=cf.headers)
-        print ("naver_original_html status : ", naver_original_html)
-        naver_html = BeautifulSoup(naver_original_html.text, "html.parser")
+        # 기사 관련 정보 크롤링
+        # "strong" 태그의 "class" 속성이 "tit_item"인 요소 추출
+        strong_tit_item = html.find_all("strong", class_="tit_item")
+        for title in strong_tit_item:
+            if title.get('title'):
+                news_agency.append(title.text.strip())
+        # "div" 태그의 "class" 속성이 "item-title"인 요소 추출
+        div_item_title = html.find_all("div", class_="item-title")
+        for article_title in div_item_title:
+            title = article_title.text.strip()
+            title = re.sub(pattern=pattern1, repl='', string=str(title))
+            title = re.sub(pattern=pattern2, repl='', string=str(title))
+            title = re.sub(pattern=pattern3, repl='', string=str(title))
+            title = re.sub(pattern=pattern4, repl='', string=str(title))
+            titles.append(title)
+            article_link.append(article_title.find("a")['href'])
+            # 기사 내용 크롤링
+            daum_original_html = requests.get(article_title.find("a")['href'], headers=cf.headers)
+            print ("daum_original_html status : ", daum_original_html)
+            daum_html = BeautifulSoup(daum_original_html.text, "html.parser")
+            # "span" 태그의 "class" 속성이 "gem-subinfo"인 요소 추출
+            div_article_view = daum_html.find("div", class_="article_view")
+            # print (div_article_view)
+            paragraphs = div_article_view.find_all('p')
+            article = ''
+            for p in paragraphs:
+                article = article + p.get_text().strip()
+            article = re.sub(pattern=pattern1, repl='', string=article)
+            article = re.sub(pattern=pattern2, repl='', string=article)
+            article = re.sub(pattern=pattern3, repl='', string=article)
+            article = re.sub(pattern=pattern4, repl='', string=article)
+            article_text.append(article)
+        # "span" 태그의 "class" 속성이 "gem-subinfo"인 요소 추출
+        div_item_contents = html.find_all("span", class_="gem-subinfo")
+        for article_date in div_item_contents:
+            article_reg_date.append(article_date.text.strip().replace(".", "-"))
 
-        # 기사 발행일
-        html_date = naver_html.select_one("div#ct> div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span")
-        if html_date is None:
-            to_remove_url.append(naver_url)
-            continue
-        news_date = html_date.attrs['data-date-time']
-        article_reg_date.append(news_date)
-
-        # 언론사
-        news_agency.append( naver_html.select_one('meta:nth-child(10)').get("content") )
-
-        # 뉴스 제목
-        title = naver_html.select_one("#ct > div.media_end_head.go_trans > div.media_end_head_title > h2")
-        if title == None:
-            title = naver_html.select_one("#content > div.end_ct > div > h2")
-
-        # 뉴스 본문
-        article = naver_html.select("article#dic_area")
-        if article == []:
-            article = naver_html.select("#articeBody")
-        article = ''.join(str(article))
-        
-        # html태그제거 및 텍스트 다듬기
-        pattern1 = '<[^>]*>'
-        pattern2 = '"'
-        pattern3 = "'"
-        pattern4 = '\t'
-
-        title = re.sub(pattern=pattern1, repl='', string=str(title))
-        title = re.sub(pattern=pattern2, repl='', string=str(title))
-        title = re.sub(pattern=pattern3, repl='', string=str(title))
-        title = re.sub(pattern=pattern4, repl='', string=str(title))
-        
-        article = re.sub(pattern=pattern1, repl='', string=article)
-        article = re.sub(pattern=pattern2, repl='', string=article)
-        article = re.sub(pattern=pattern3, repl='', string=article)
-        article = re.sub(pattern=pattern4, repl='', string=article)
-        
-        titles.append(title)
-        article_text.append(article)
-        
-    for remove_url in to_remove_url:
-        article_link.remove(remove_url)
-    
     if "+" in query:
         company_name = query.split("+")[0]
     else:
@@ -267,11 +249,6 @@ def daum_news_crawler(maxpage, query, sort, crawling_date_id, portal_name):
     df = pd.DataFrame(result)  # df로 변환
     df = df.drop_duplicates()
 
-    # # 새로 만들 파일이름 지정
-    # now = datetime.now()  # 파일이름 현 시간으로 저장하기
-    # outputFileName = f'articles_from_naver_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.xlsx'
-    # df.to_excel('/Users/kakao/Downloads/'+outputFileName,sheet_name='sheet1')
-    
     # DB에 delete - insert
     conn = cf.connect_to_db()
     cursor = conn.cursor()
