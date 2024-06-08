dde0ca359c31dc513f2d07dc8b69615abd182009
fleamon <fleamon@nate.com>
2024-06-09 07:53:34 +0900

    update

--- DailyMain.py
+++ DailyMain.py
@@ -26,34 +26,53 @@ def DailyMain():
     # company_ceo_name_list = ['카카오+김범수', '삼성전자+이재용', '현대자동차+정의선', 'LG+구광모', 'SK+최태원']
     # company_ceo_name_list = ['카카오+김범수']
     # company_ceo_name_list = ['삼성전자+이재용']
-    company_ceo_name_list = ['한진+조원태', '대한항공+조원태', '한진칼+조원태']
-    # company_ceo_name_list = ['한진+조원태']
+    # company_ceo_name_list = ['한진+조원태', '대한항공+조원태', '한진칼+조원태']
     # company_ceo_name_list = ['한진칼,한진,대한항공+조원태']
+    # company_ceo_name_list = ['하이브+방시혁']
+    company_ceo_name_list = ['포스코+장인화']
     for company_ceo_name in company_ceo_name_list:
-        start_date = "2024.05.20"
-        # start_date = "2024.03.27"
-        end_date = "2019.01.01"
+        start_date = "2024.06.08"
+        # end_date = "2023.01.01"  # 하이브
+        end_date = "2024.01.01"  # 포스코
         is_public = True
 
         # article crawling (완료) (커뮤니티 점검필요)
         # crawl.crawling_articles_from_keyword(company_ceo_name, formatted_yesterday, formatted_yesterday, is_public)
-        # crawl.crawling_articles_from_keyword(company_ceo_name, start_date, end_date, is_public)
+        crawl.crawling_articles_from_keyword(company_ceo_name, start_date, end_date, is_public)
 
         # dart 공시자료 추가
         # dart.main(company_ceo_name, formatted_yesterday, formatted_yesterday)
         # dart.main(company_ceo_name, start_date, end_date)
+        # start_date = "2020.11.30"
+        # end_date = "2020.11.30"
+        # dart.main(company_ceo_name, start_date, end_date)
+        # start_date = "2020.11.30"
+        # end_date = "2020.11.01"
+        # dart.main(company_ceo_name, start_date, end_date)
+        # start_date = "2020.03.31"
+        # end_date = "2020.03.01"
+        # dart.main(company_ceo_name, start_date, end_date)
+        # start_date = "2019.05.31"
+        # end_date = "2019.05.01"
+        # dart.main(company_ceo_name, start_date, end_date)
+        # start_date = "2019.02.28"
+        # end_date = "2019.01.01"
+        # dart.main(company_ceo_name, start_date, end_date)
+        # start_date = "2018.11.30"
+        # end_date = "2018.11.01"
+        # dart.main(company_ceo_name, start_date, end_date)
 
         # 특정날짜 특정기업 기사 한줄요약 (완료)
         # summaize_articles.main(company_ceo_name, formatted_yesterday, formatted_yesterday)
-        # summaize_articles.main(company_ceo_name, start_date, end_date)
+        summaize_articles.main(company_ceo_name, start_date, end_date)
         
         # 키워드비교 스코어링 (완료)
         # compare_scoring.main(company_ceo_name, formatted_yesterday, formatted_yesterday)
-        # compared_articles.main(company_ceo_name, start_date, end_date)
+        compared_articles.main(company_ceo_name, start_date, end_date)
 
         # bert 스코어링 결과 저장 (완료)
         # bert_scoring.main(company_ceo_name, formatted_yesterday, formatted_yesterday)
-        # bert_scoring.main(company_ceo_name, start_date, end_date)
+        bert_scoring.main(company_ceo_name, start_date, end_date)
         
         # keyword로 스코어링 결과 저장 (완료)
         # keyword_scoring.main(company_ceo_name, formatted_yesterday, formatted_yesterday)
