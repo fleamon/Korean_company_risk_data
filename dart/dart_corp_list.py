77f56b2cb160330d29ab56f70a8ee9d7ef8598b9
Bass.won <bass.won@kakaocorp.com>
2024-06-16 05:15:04 +0900

    dart company name, ceo name FIN

--- dart/dart_corp_list.py
+++ dart/dart_corp_list.py
@@ -1,11 +1,16 @@
 import CommonFunction as cf
 import dart_fss as dart_fss
+from dart_fss import get_corp_list
 import pandas as pd
+import requests
 
 
 def main():
     dart_fss.set_api_key(api_key=cf.DART_API_KEY)
     # print (dart_fss)
+
+    corp_list = get_corp_list()
+    api_url = "https://opendart.fss.or.kr/api/company.json"
 
     all_corp = dart_fss.api.filings.get_corp_code()
     # print (len(all_corp))
@@ -15,20 +20,14 @@ def main():
 
     # 상장
     df_listed = all_corp_df[all_corp_df['stock_code'].notnull()]
-    print (len(df_listed))
-    print (df_listed)
+    # print (len(df_listed))
+    # print (df_listed)
     df_listed_columns = ','.join(df_listed.columns.to_list())
-    print (df_listed_columns)
 
     # 비상장
     df_non_listed = all_corp_df[all_corp_df['stock_code'].isnull()]
     # print (len(df_non_listed))
     # print (df_non_listed)
-
-    # corp_code = df_listed[df_listed['corp_name'] == '카카오']
-    # print (corp_code)
-    # print (dart_fss.api.filings.get_corp_info(corp_code))
-    # print (type(dart_fss.api.filings.get_corp_info(corp_code)))
 
     conn = cf.connect_to_db()
     cursor = conn.cursor()
@@ -37,10 +36,11 @@ def main():
     create_table_query = """
     CREATE TABLE IF NOT EXISTS Korean_company_risk_data_meta.company_list (
     seq bigint NOT NULL AUTO_INCREMENT,
-    corp_code varchar(50) DEFAULT NULL COMMENT '기업명',
-    corp_name varchar(50) DEFAULT NULL COMMENT '제목Score',
-    stock_code varchar(50) DEFAULT NULL COMMENT '기사Score',
-    modify_date varchar(50) DEFAULT NULL COMMENT '기사Score',
+    corp_code varchar(50) DEFAULT NULL COMMENT 'dart기업코드',
+    corp_name varchar(50) DEFAULT NULL COMMENT '기업이름,
+    ceo_name varchar(50) DEFAULT NULL COMMENT 'ceo이름',
+    stock_code varchar(50) DEFAULT NULL COMMENT '주식코드',
+    modify_date varchar(50) DEFAULT NULL COMMENT 'dart수정일',
     load_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '저장시간',
     PRIMARY KEY (seq),
     KEY idx_corp_name (corp_name)
@@ -60,11 +60,19 @@ def main():
     for index, row in df_listed.iterrows():
         # print (row.name)
         # print (row)
+        params = {
+            'crtfc_key': cf.DART_API_KEY,
+            'corp_code': row["corp_code"]
+        }
+        response = requests.get(api_url, params=params)
+        data = response.json()
+        ceo_name = data['ceo_nm']
+        print (corp_list.find_by_corp_code(row["corp_code"]))
         insert_query = f'''
             INSERT INTO Korean_company_risk_data_meta.company_list
             ({df_listed_columns}, load_date)
             VALUES
-            ('{row["corp_code"]}', '{row["corp_name"]}',
+            ('{row["corp_code"]}', '{row["corp_name"]}', '{ceo_name}',
             '{row["stock_code"]}','{row["modify_date"]}', NOW())
             ON DUPLICATE KEY UPDATE 
             corp_name=VALUES(corp_name)
